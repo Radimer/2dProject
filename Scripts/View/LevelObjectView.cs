5cacc9a8950a8d9f87aaf303c72fcaa21b5b0569
evdokimov_s <radimer18021992@gmail.com>
2022-09-05 03:56:56 +0300

    initial

---
+++ Scripts/View/LevelObjectView.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PlatformerMVC
+{
+    public class LevelObjectView : MonoBehaviour
+    {
+        public Transform _transform;
+        public SpriteRenderer _spriteRender;
+        public Collider2D _collider;
+        public Rigidbody2D _rb;
+    }
+}
